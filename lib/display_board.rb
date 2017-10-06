e4b4d32e9442391ded6074743db335829ac28e18
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 19:09:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,15 @@
 def display_board(board)
-  puts "   | "X" |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
 end
