e03388da9d3d16aeaa8701730a8b1e758ceee3e0
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 17:35:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@ board = [" X ", " X ", " X ", " X ", " X ", " X ", " X ", " X ", " X "]
 board2 = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board(board)
   puts "#{board[0]}"
-  puts "#{board2[0]}"
+  puts "#{board2[1]}"
 end
