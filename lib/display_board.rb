302d9b53f44da4c230cec467853eef04603d9f40
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 18:57:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -14,7 +14,7 @@ board = row1, row2, row3, row4, row5
 
 
 def display_board(board)
-  puts "#{board}", "#{row1[0]}", "#{row1[1]}", "#{row1[2]}", "#{row1[3]}", "#{row1[4]}"
-  puts "#{row3[0]}", "#{row3[1]}", "#{row3[2]}", "#{row3[3]}", "#{row3[4]}"
-  puts "#{row5[0]}", "#{row5[1]}", "#{row5[2]}", "#{row5[3]}", "#{row5[4]}"
+  puts "#{board}","#{row1[0]}", "#{row1[1]}", "#{row1[2]}", "#{row1[3]}", "#{row1[4]}"
+  puts "#{board}","#{row3[0]}", "#{row3[1]}", "#{row3[2]}", "#{row3[3]}", "#{row3[4]}"
+  puts "#{board}","#{row5[0]}", "#{row5[1]}", "#{row5[2]}", "#{row5[3]}", "#{row5[4]}"
 end
