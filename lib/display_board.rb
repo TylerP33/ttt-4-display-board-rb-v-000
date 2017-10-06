f4f413adccb0d0b8a6b948c2fcad779cdc16347d
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 18:52:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,10 +4,7 @@ row3 = ["   ", "|", " 0 ", "|", "   "]
 row4 = ["-----------"]
 row5 = ["   ", "|", "   ", "|", " 0 "]
 
-board = "#{row1[0]}", "#{row1[1]}", "#{row1[2]}", "#{row1[3]}", "#{row1[4]}"
-
-
 
 def display_board(board)
-  puts "#{board} is now won by O"
+  puts "#{row1[0]}", "#{row1[1]}", "#{row1[2]}", "#{row1[3]}", "#{row1[4]}"
 end
