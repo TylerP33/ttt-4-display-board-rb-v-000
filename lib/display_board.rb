9434869bae454a701142dc2ca87498f7a518ae8c
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 18:34:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,14 +10,4 @@ def display_board(board)
   puts "#{board}"
 end
 
-row1 = ["   ", "|", "   ", "|", "   "]
-row2 = ["-----------"]
-row3 = ["   ", "|", " X ", "|", "   "]
-row4 = ["-----------"]
-row5 = ["   ", "|", "   ", "|", "   "]
 
-board = row1, row2, row3, row4, row5
-
-def display_board(board)
-  puts "#{board}"
-end
