f0f752d586f8bd3138d24d112e02d105c16afab4
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 18:53:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,6 +4,12 @@ row3 = ["   ", "|", " 0 ", "|", "   "]
 row4 = ["-----------"]
 row5 = ["   ", "|", "   ", "|", " 0 "]
 
+row1
+row2
+row3
+row4
+row5
+
 
 def display_board(row1)
   puts "#{row1[0]}", "#{row1[1]}", "#{row1[2]}", "#{row1[3]}", "#{row1[4]}"
