c883e03f9f33306c679d23007840e6d39774bf60
Tyler Pulse <tyler3321@gmail.com>
2017-10-06 19:31:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,20 +1,7 @@
-row1 = puts "   |   |   "
-row2 = puts "-----------"
-row3 = puts "   |   |   "
-row4 = puts "-----------"
-row5 = puts "   |   |   "
-
-board = "row1, row2, row3, row4, row5"
-
 def display_board(board)
   row1 = puts "   |   |   "
   row2 = puts "-----------"
   row3 = puts "   |   |   "
   row4 = puts "-----------"
   row5 = puts "   |   |   "
-
-
-
-
-
 end
